7f22c4a6144f9b6c0e21a99e8d43f2019cfa73fa
Moh Al-Zeyadi <moh.al-zeyadi@cognitran.com>
2022-07-28 15:08:28 +0000

    commit 8th

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -146,6 +146,7 @@ def get_pipeline(
     pipeline_session = get_pipeline_session(region, default_bucket)
 
     # Parameters for pipeline execution
+    training_epochs = ParameterString(name="TrainingEpochs", default_value="100")
     processing_instance_count = ParameterInteger(name="ProcessingInstanceCount", default_value=1)
     training_instance_type = ParameterString(name="TrainingInstanceType", default_value="ml.m5.xlarge")
     model_approval_status = ParameterString(name="ModelApprovalStatus",default_value="PendingManualApproval",  # ModelApprovalStatus can be set to a default of "Approved" if you don't want manual approval.
@@ -222,7 +223,7 @@ def get_pipeline(
 
 
     # training step parameters
-    training_epochs = ParameterString(name="TrainingEpochs", default_value="100")
+   
     
     from sagemaker.tensorflow import TensorFlow
     from sagemaker.inputs import TrainingInput
