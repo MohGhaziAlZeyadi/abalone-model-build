1fa77bc618d0da281fc5f7cbf75ba101a1e8f2a2
Moh Al-Zeyadi <moh.al-zeyadi@cognitran.com>
2022-07-29 15:06:20 +0000

    Commit 37

--- pipelines/abalone/.ipynb_checkpoints/preprocess-checkpoint.py
+++ pipelines/abalone/.ipynb_checkpoints/preprocess-checkpoint.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
         "medianIncome",
     ]
     ]
-    Y = df[["medianHouseValue"]] / 100000
+    Y = int(df[["medianHouseValue"]] / 100000)
 
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33)
 
