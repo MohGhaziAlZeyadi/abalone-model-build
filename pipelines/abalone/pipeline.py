3ededda7df75d9022d2a429b09cb002c9399de57
Moh Al-Zeyadi <moh.al-zeyadi@cognitran.com>
2022-07-30 14:50:28 +0000

    Commit 54

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -380,7 +380,7 @@ def get_pipeline(
             property_file=evaluation_report,
             json_path="binary_classification_metrics.accuracy.value",  # This should follow the structure of your report_dict defined in the evaluate.py file.
         ),
-        right=0.5,  # You can change the threshold here
+        right=0.4,  # You can change the threshold here
     )
     step_cond = ConditionStep(
         name="CheckMSEAbaloneEvaluation",
