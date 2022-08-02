3a5606c06a21a61ed4611a137ef22d0c6b5e1889
Moh Al-Zeyadi <moh.al-zeyadi@cognitran.com>
2022-08-02 16:20:40 +0000

    Commit 123

--- pipelines/abalone/.ipynb_checkpoints/evaluate-checkpoint.py
+++ pipelines/abalone/.ipynb_checkpoints/evaluate-checkpoint.py
@@ -15,7 +15,7 @@ def install(package):
 if __name__ == "__main__":
 
     #install("tensorflow==2.4.1")
-    install("tensorflow==1.5.0")
+    install("tensorflow==1.15.0")
     install("numpy==1.21.0")
 
     
