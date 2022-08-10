46b9b4d52e73e963fdcbf8dae67e39c1930bd78a
Moh Al-Zeyadi <moh.al-zeyadi@cognitran.com>
2022-08-10 14:38:31 +0000

    Commit 40

--- pipelines/abalone/evaluate.py
+++ pipelines/abalone/evaluate.py
@@ -51,47 +51,6 @@ def model_summary(model):
     # lastlayer by name
     print((model.get_layer("lastlayer").weights))
 
-def parse_args():
-
-    parser = argparse.ArgumentParser()
-
-    # hyperparameters sent by the client are passed as command-line arguments to the script
-    parser.add_argument('--epochs', type=int, default=1)
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--learning_rate', type=float, default=0.01)
-
-    # data directories
-    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
-    
-    parser.add_argument('--test', type=str, default=os.environ.get('SM_CHANNEL_TEST'))
-
-    # model directory
-    parser.add_argument('--sm-model-dir', type=str, default=os.environ.get('SM_MODEL_DIR')) #  A string that represents the path where the training job writes the model artifacts to. After training, artifacts in this directory are uploaded to S3 for model hosting.
-
-    return parser.parse_known_args()
-
-
-def get_train_data(train_dir):
-
-    x_train = np.load(os.path.join(train_dir, 'x_train.npy'))
-    y_train = np.load(os.path.join(train_dir, 'y_train.npy'))
-    print('x train', x_train.shape,'y train', y_train.shape)
-    print(type(x_train))
-    print(type(y_train))
-
-    return x_train, y_train
-
-
-def get_test_data(test_dir):
-
-    x_test = np.load(os.path.join(test_dir, 'x_test.npy'))
-    y_test = np.load(os.path.join(test_dir, 'y_test.npy'))
-    print('x test', x_test.shape,'y test', y_test.shape)
-    print(type(x_test))
-    print(type(y_test))
-
-    return x_test, y_test
-
 
 def get_model():
     model = Sequential()
@@ -115,18 +74,17 @@ if __name__ == "__main__":
     
     install("numpy==1.19.2")
     
-
-    args, _ = parse_args()
-
-    print('Training data location: {}'.format(args.train))
-    print('Test data location: {}'.format(args.test))
-    x_train, y_train = get_train_data(args.train)
+    train_path = "/opt/ml/processing/train/"
+    x_train = np.load(os.path.join(train_path, "x_train.npy"))
+    y_train = np.load(os.path.join(train_path, "y_train.npy"))
     
-    x_test, y_test = get_test_data(args.test)
+    test_path = "/opt/ml/processing/test/"
+    x_test = np.load(os.path.join(test_path, "x_test.npy"))
+    y_test = np.load(os.path.join(test_path, "y_test.npy"))
 
-    batch_size = args.batch_size
-    epochs = args.epochs
-    learning_rate = args.learning_rate
+    batch_size = 64
+    epochs = 3
+    learning_rate = 0.01
     print('batch_size = {}, epochs = {}, learning rate = {}'.format(batch_size, epochs, learning_rate))
 
 
