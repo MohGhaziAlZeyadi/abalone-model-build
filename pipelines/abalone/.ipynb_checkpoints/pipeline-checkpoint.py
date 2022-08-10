aa8be571695c1aa803f9696b3ecc660e3a9bc9a2
Moh Al-Zeyadi <moh.al-zeyadi@cognitran.com>
2022-08-10 08:37:52 +0000

    Commit 29

--- pipelines/abalone/.ipynb_checkpoints/pipeline-checkpoint.py
+++ pipelines/abalone/.ipynb_checkpoints/pipeline-checkpoint.py
@@ -23,11 +23,7 @@ Implements a get_pipeline(**kwargs) method.
 
 """
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-    
-    
-install("sagemaker==2.91.1")
+
 
 import os
 import boto3
