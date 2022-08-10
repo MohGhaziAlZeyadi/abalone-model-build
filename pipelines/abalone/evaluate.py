465d2d01cc72d66547f5a1c89c441881ad9f53ca
Moh Al-Zeyadi <moh.al-zeyadi@cognitran.com>
2022-08-10 14:15:00 +0000

    Commit 39

--- pipelines/abalone/evaluate.py
+++ pipelines/abalone/evaluate.py
@@ -51,6 +51,60 @@ def model_summary(model):
     # lastlayer by name
     print((model.get_layer("lastlayer").weights))
 
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the script
+    parser.add_argument('--epochs', type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--learning_rate', type=float, default=0.01)
+
+    # data directories
+    parser.add_argument('--train', type=str, default=os.environ.get('SM_CHANNEL_TRAIN'))
+    
+    parser.add_argument('--test', type=str, default=os.environ.get('SM_CHANNEL_TEST'))
+
+    # model directory
+    parser.add_argument('--sm-model-dir', type=str, default=os.environ.get('SM_MODEL_DIR')) #  A string that represents the path where the training job writes the model artifacts to. After training, artifacts in this directory are uploaded to S3 for model hosting.
+
+    return parser.parse_known_args()
+
+
+def get_train_data(train_dir):
+
+    x_train = np.load(os.path.join(train_dir, 'x_train.npy'))
+    y_train = np.load(os.path.join(train_dir, 'y_train.npy'))
+    print('x train', x_train.shape,'y train', y_train.shape)
+    print(type(x_train))
+    print(type(y_train))
+
+    return x_train, y_train
+
+
+def get_test_data(test_dir):
+
+    x_test = np.load(os.path.join(test_dir, 'x_test.npy'))
+    y_test = np.load(os.path.join(test_dir, 'y_test.npy'))
+    print('x test', x_test.shape,'y test', y_test.shape)
+    print(type(x_test))
+    print(type(y_test))
+
+    return x_test, y_test
+
+
+def get_model():
+    model = Sequential()
+    #Input Layer
+    model.add(Dense(8, activation='relu', input_dim = 8, name="1stlayer"))
+
+    #Hidden Layer
+    model.add(Dense(64,kernel_initializer='normal', activation='relu', name="2ndlayer"))
+    model.add(Dense(32,kernel_initializer='normal', activation='relu', name="3rdlayer"))
+    #Output Layer
+    model.add(Dense(1,kernel_initializer='normal', activation = 'relu',  name="lastlayer"))
+    
+    return model
 
 
 
@@ -59,35 +113,40 @@ if __name__ == "__main__":
     install("tensorflow==2.4.1")
     
     
-    model_path = f"/opt/ml/processing/model/model.tar.gz"
-    with tarfile.open(model_path, "r:gz") as tar:
-        tar.extractall("./model")
-        
-        
-    import tensorflow as tf
-    from tensorflow import keras
-    from tensorflow.keras import optimizers
+    install("numpy==1.19.2")
+    
 
-    model_loded = tf.keras.models.load_model("./model/1")
-    #model_loded = tf.keras.models.load_model("./model/1", custom_objects=None, compile=True, options=None)
-    #model_loded.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
-    print(model_loded.summary())
+    args, _ = parse_args()
+
+    print('Training data location: {}'.format(args.train))
+    print('Test data location: {}'.format(args.test))
+    x_train, y_train = get_train_data(args.train)
     
-    model_summary(model_loded)
+    x_test, y_test = get_test_data(args.test)
+
+    batch_size = args.batch_size
+    epochs = args.epochs
+    learning_rate = args.learning_rate
+    print('batch_size = {}, epochs = {}, learning rate = {}'.format(batch_size, epochs, learning_rate))
+
+
+    model = get_model()
     
     
-    test_path = "/opt/ml/processing/test/"
-    x_test = np.load(os.path.join(test_path, "x_test.npy"))
-    y_test = np.load(os.path.join(test_path, "y_test.npy"))
+    ##Compile the network 
+    ##optimizer = tf.keras.optimizers.SGD(learning_rate)
+    #model.compile(loss='mean_squared_error',optimizer='adam')
+    ##model.compile(optimizer=optimizer, loss='mse')
+
+    model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
+   
     
-    print('x test', x_test.shape,'y test', y_test.shape)
-    print(type(x_test))
-    print(type(y_test))
     
     
-    print("Evalaution Start...")
-    batch_size = 64
-    scores = model_loded.evaluate(x_test, y_test,  batch_size, verbose=1)
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
+
+    # evaluate on test set
+    scores = model.evaluate(x_test, y_test, batch_size, verbose=1)
     print("\nTest MSE :", scores)
 
     # Available metrics to add to model: https://docs.aws.amazon.com/sagemaker/latest/dg/model-monitor-model-quality-metrics.html
